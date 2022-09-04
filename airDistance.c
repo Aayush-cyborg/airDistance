b5f0d5308a2c785e8e11c6edee71ca28e1261178
Aayush-cyborg <akulkarni3@unl.edu>
2022-09-04 12:51:14 +0000

    added some code

--- airDistance.c
+++ airDistance.c
@@ -3,32 +3,32 @@
 
 //main method
 int main(){
-    double Olongitude,Olatitude,longitude,latitude,distance = 0.0;
-    //asking the user the latitude {of orgin}
-    printf("Enter Latitude of origin in degrees: \n");
-    scanf("%lf: ",&Olatitude);
-    //asking the user the longitudes {of orgin}
-    printf("Enter Longitude of origin in degrees: \n");
-    scanf("%lf: ",&Olongitude);
-    //asking the user the latitude {of destination}
-    printf("Enter Latitude of Destination in degrees: \n");
-    scanf("%lf: ",&latitude);
-    //asking the user the longitudes {of destination}
-    printf("Enter Longitude of Destination in degrees: \n");
-    scanf("%lf: ",&longitude);
+
+    double Orlongitude,Orlatitude,dlongitude,dlatitude,distance = 0.0;
+    printf("Enter Latitude of origin: \n");
+    scanf("%lf: ",&Orlatitude);//asking the user the latitude {of orgin}
+
+    printf("Enter Longitude of origin: \n");
+    scanf("%lf: ",&Orlongitude);//asking the user the longitudes {of orgin}
+
+    printf("Enter Latitude of Destination: \n");
+    scanf("%lf: ",&dlatitude);//asking the user the latitude {of destination}
+
+    printf("Enter Longitude of Destination: \n");
+    scanf("%lf: ",&dlongitude);//asking the user the longitudes {of destination}
 
     //defining the value of pi
     double pi = 3.1415926;
     //defining the value of earth's radius
     double earth = 6371.0;
     //writing the formula using library functions of math.h
-    distance = acos(sin(Olatitude*pi/180.0)*sin(latitude*pi/180.0)+cos(Olatitude*pi/180.0)*
-    cos(latitude*pi/180.0)*cos((longitude - Olongitude)*pi/180.0))*earth; //converting the degrees to radians
+    distance = acos(sin(Orlatitude*pi/180.0)*sin(dlatitude*pi/180.0)+cos(Orlatitude*pi/180.0)*
+    cos(dlatitude*pi/180.0)*cos((dlongitude - Orlongitude)*pi/180.0))*earth; //converting the degrees to radians
 
     //printing the output: Location Distance containing [Origin, Destination and Air distance]
     printf("\nLocation Distance: \n===============================\n");
-    printf("Origin: (%lf,%lf)\n",Olatitude,Olongitude);
-    printf("Destination: (%lf,%lf)\n",latitude,longitude);
+    printf("Origin: (%lf,%lf)\n",Orlatitude,Orlongitude);
+    printf("Destination: (%lf,%lf)\n",dlatitude,dlongitude);
     printf("Air Distance is %lf kms\n",distance);
 
     return 0;
